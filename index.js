addbe736eb4010acb0578f99120858ed569cd7a9
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-06-07 03:09:10 +0500

    added 6 templates

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
-// $(document).ready(function () {
-//   $("#btn-login").click(function () {
-//     $("#background").show();
-//     $("#popup-wrapper").show();
-//   });
-//   $("#cross").click(function () {
-//     $("#background").hide();
-//     $("#popup-wrapper").hide();
-//   });
-//   $("#background").click(function () {
-//     $(this).hide();
+$(document).ready(function () {
+  // popup-8 select option poppup
+  $("#popup-8-option-1 ").click(function () {
+    $(this).toggleClass("option-selected-second");
+    $(".option-1-text-color").toggleClass("popup-8-text");
 
-//     $("#popup-wrapper").hide();
-//   });
-// });
+    $("#popup-8-choice-1").toggle();
+  });
+
+  $("#popup-8-option-2").click(function () {
+    $(this).toggleClass("option-selected-second");
+    $(".option-2-text-color").toggleClass("popup-8-text");
+    $("#popup-8-choice-2").toggle();
+  });
+
+  $("#popup-8-option-3").click(function () {
+    $(this).toggleClass("option-selected-second");
+    $(".option-3-text-color").toggleClass("popup-8-text");
+    $("#popup-8-choice-3").toggle();
+  });
+
+  $("#popup-8-option-4").click(function () {
+    $(this).toggleClass("option-selected-second");
+    $(".option-4-text-color").toggleClass("popup-8-text");
+    $("#popup-8-choice-4").toggle();
+  });
+
+  // popup-12 select option poppup
+  $("#popup-12-option-1 ").click(function () {
+    $(this).toggleClass("option-selected");
+
+    $("#popup-12-option-1 >input").prop(
+      "checked",
+      !$("#popup-12-option-1 >input").prop("checked")
+    );
+  });
+
+  $("#popup-12-option-2 ").click(function () {
+    $(this).toggleClass("option-selected");
+
+    $("#popup-12-option-2 >input").prop(
+      "checked",
+      !$("#popup-12-option-2 >input").prop("checked")
+    );
+  });
+
+  $("#popup-12-option-3 ").click(function () {
+    $(this).toggleClass("option-selected");
+
+    $("#popup-12-option-3 >input").prop(
+      "checked",
+      !$("#popup-12-option-3 >input").prop("checked")
+    );
+  });
+
+  $("#popup-12-option-4 ").click(function () {
+    $(this).toggleClass("option-selected");
+
+    $("#popup-12-option-4 >input").prop(
+      "checked",
+      !$("#popup-12-option-4 >input").prop("checked")
+    );
+  });
+});
